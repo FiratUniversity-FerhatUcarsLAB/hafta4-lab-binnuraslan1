34b913aa299bf17557fbdcba142e5d905cf1cdb7
binnuraslan1 <binnuraslan888@gmail.com>
2025-11-06 11:40:48 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,60 +1,42 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: [Binnur Aslan]
+ * Ogrenci No: [250541084]
+ * Tarih: [06.11.2025]
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
  * 
  * Bu program kullanicidan ogrenci bilgilerini alir ve
  * duzenli bir formatta ekrana yazdirir.
- * Diğer java dosyalarının başında da bu örnek formattaki gibi kısa bilgi giriniz
- */
 
-import java.util.Scanner;
+     import java.util.Scanner;
 
-public class OgrenciBilgi {
-    public static void main(String[] args) {
-        // Scanner objesi olusturun
-        
-        
-        // Degisken tanimlamalari
-        // String ad, soyad;
-        // int ogrenciNo, yas;
-        // double gpa;
-        
-        
-        // Kullanicidan bilgileri alin
-        System.out.println("=== OGRENCI BILGI SISTEMI ===");
-        System.out.println();
-        
-        // Ad
-        
-        
-        // Soyad
-        
-        
-        // Ogrenci No
-        
-        
-        // Yas
-        
-        
-        // GPA
-        
-        
-        // Bilgileri ekrana yazdirin
-        System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
-        
-        // COZUMUNUZU BURAYA YAZIN
-        
-        
-        
-        
-        
-        
+        void main() {
+            Scanner input = new Scanner(System.in);
 
-        
-        
-        // Scanner'i kapatin (önemli pratik)
-        
-    }
-}
+            System.out.print("Ad: ");
+            String ad = input.nextLine();
+
+            System.out.print("Soyad: ");
+            String soyad = input.nextLine();
+
+            System.out.print("Ogrenci No: ");
+            int ogrNo = input.nextInt();
+
+            System.out.print("Yas: ");
+            int yas = input.nextInt();
+
+            input.useLocale(Locale.US);
+            System.out.print("GPA (0.00 - 4.00): ");
+            double gpa = input.nextDouble();
+
+            String durum = (gpa >= 2.0) ? "Basarili Ogrenci" : "Basarisiz Ogrenci";
+
+            System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
+            System.out.println("Ad Soyad: " + ad + " " + soyad);
+            System.out.println("Ogrenci No: " + ogrNo);
+            System.out.println("Yas: " + yas);
+            System.out.println("GPA: " + String.format("%.2f", gpa));
+            System.out.println("Durum: " + durum);
+
+            input.close();
+        }
+
